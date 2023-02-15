2ce7d3b47b29db927651277de25569571c27255f
Booorgee <40717544+Booorgee@users.noreply.github.com>
2023-02-15 14:53:53 +0800

    Update main.py
    Added user defined functions in FLAMES

--- main.py
+++ main.py
@@ -41,26 +41,36 @@ def LoveLetters():
     return
 
 def FLAMES():
-    flames = {1: "Friends", 2: "Lovers",
+    def compare(name1, name2):
+		for i in name1:
+			if i in name2:
+				name1 = name1.replace(i, "")
+				name2 = name2.replace(i, "")
+				
+		length = len(name1) + len(name2)
+		
+		
+		return length
+	
+	def nameInput():
+		x = input().replace(" ", "").lower()
+
+		y = input().replace(" ", "").lower()
+		
+		return x, y
+	
+	flames = {1: "Friends", 2: "Lovers",
 		3: "Affectionate", 4: "Marriage",
 		0: "Enemies",} 
-
-
-    name1 = input().replace(" ", "").lower()
-
-    name2 = input().replace(" ", "").lower()
-
-
-    for i in name1:
-        if i in name2:
-            name1 = name1.replace(i, "")
-            name2 = name2.replace(i, "")
-
-
-    length = len(name1) + len(name2)
-    print(length, flames[length % 5])
-    
-    return
+		
+	name1, name2 = nameInput()
+	
+	length = compare(name1, name2)
+	
+	
+	print(length, flames[length % 5])
+	
+	return
 
 def DDayAtValentines():
     successStr = 'mahal na ata kita hehe'
